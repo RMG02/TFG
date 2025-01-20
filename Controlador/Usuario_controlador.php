127532dc52bcf90bd2d958248634adfb260c944c
Diegor897 <dchocano@ucm.es>
2025-01-20 17:30:48 +0100

    creacion seguidores y siguiendo

--- Controlador/Usuario_controlador.php
+++ Controlador/Usuario_controlador.php
@@ -33,6 +33,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             'nick' => $_POST['nick'],
             'email' => $_POST['email'],
             'password' => $_POST['password'],
+            'seguidores' =>[],
+            'siguiendo' =>[],
             'admin' => false
 
         ];
